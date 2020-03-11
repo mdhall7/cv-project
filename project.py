02aa265f5197532a89ee7456a24a1817dfb24883
AdamHrrrahn <Adam>
2020-03-11 10:12:25 -0600

    updating project.py

--- project.py
+++ project.py
@@ -27,6 +27,7 @@ def show_image (canvas, file_name):
     canvas.image=image
     canvas.origin_image = image
     canvas.display = image
+    canvas.last = image
     canvas.has_image = True
 
 def get_file(canvas, file_menu):
@@ -39,20 +40,66 @@ def clear_image(canvas):
     canvas.image=image
     canvas.origin_image = image
     canvas.display = image
+    canvas.last = image
     canvas.has_image = False
 
 def get_masks(image):
     pass
 
-def select(canvas, menu):
-    pass
+def select(canvas, menu, event):
+    x, y = event.x, event.y
+    for i in range(length(canvas.mask_list)):
+        if (canvas.mask_list[i][y][x]):
+            canvas.selected = i
+    # canvas.selected_mask = make_selected_mask(canvas.mask_list[i])
+
+
 
 def deselect(canvas, menu):
     pass
 
+def make_selected_mask(mask_list):
+    pass
+    # find countours and draw them as a white outline on a black background
+
 # def reset_image(canvas, menu):
     # canvas.image = canvas.origin_image
 #     menu['Reset image'].entryconfig()
+
+def set_texture(canvas, menu):
+    # texture = get_texture(canvas.image.shape)
+    canvas.undo_list.insert(0,canvas.display)
+    if(length(canvas.undo_list) > 5):
+        canvas.undo_list = canvas.undo_list[:5]
+    canvas.redo_list = []
+    canvas.dispay = (1-canvas.mask_list[canvas.selected])*canvas.image + canvas.mask_list[canvas.selected]*texture
+    menu.entryconfig('Redo', state= 'disable')
+    menu.entryconfig('Undo', state= 'normal')
+
+def get_texture(shape):
+    a, b, _ = shape
+    pass
+
+def undo_change(canvas, menu):
+    canvas.redo_list.insert(0, canvas.display)
+    canvas.display = canvas.undo_list[0]
+    canvas.undo_list = canvas.undo_list[1:]
+    if(length(canvas.undo_list) == 0):
+        menu.entryconfig('Undo', state= 'disable')
+    if(length(canvas.redo_list) > 5):
+        canvas.redo_list = canvas.redo_list[:5]
+    menu.entryconfig('Redo', state= 'normal')
+
+
+def redo_chage(canvas, menu):
+    canvas.undo_list.insert(0, canvas.display)
+    canvas.display = canvas.redo_list[0]
+    canvas.redo_list = canvas.redo_list[1:]
+    if(length(canvas.redo_list) == 0):
+        menu.entryconfig('Redo', state= 'disable')
+    if(length(canvas.undo_list) > 5):
+        canvas.undo_list = canvas.undo_list[:5]
+    menu.entryconfig('Undo', state= 'normal')
 
 def main():
     window = tkinter.Tk()
@@ -66,21 +113,30 @@ def main():
     canvas.origin_image = image
     canvas.mask_list = []
     canvas.select_mask = image
+    canvas.selected = -1
+    canvas.undo_list = []
+    canvas.redo_list = []
     canvas.has_image = False
-    canvas.bind('<Button-1>', select(canvas, menu))
-    canvas.bind('<Button-3', deselect(canvas, menu))
+    # canvas.bind('<Button-1>', select(canvas, menu))
+    # canvas.bind('<Button-3', deselect(canvas, menu))
     canvas.pack(fill="both", expand="yes")
     file_menu.add_command(label = 'Clear image', command = lambda: clear_image(canvas))
-    file_menu.add_command(label = 'Room 1', command= lambda: show_image(canvas, 'room1.jpg'))#cv2.resize(cv2.imread('room1.jpg'), (800, 800))))
-    file_menu.add_command(label = 'Room 2', command= lambda: show_image(canvas, 'room2.jpeg'))
-    file_menu.add_command(label = 'Room 3', command= lambda: show_image(canvas, 'room3.jpg'))
-    file_menu.add_command(label = 'Room 4', command= lambda: show_image(canvas, 'room4.jpg'))
+    file_menu.add_command(label = 'show Room 1', command= lambda: show_image(canvas, 'room1.jpg'))#cv2.resize(cv2.imread('room1.jpg'), (800, 800))))
+    file_menu.add_command(label = 'show Room 2', command= lambda: show_image(canvas, 'room2.jpeg'))
+    file_menu.add_command(label = 'show Room 3', command= lambda: show_image(canvas, 'room3.jpg'))
+    file_menu.add_command(label = 'show Room 4', command= lambda: show_image(canvas, 'room4.jpg'))
     file_menu.add_command(label = 'choose new file', command= lambda: get_file(canvas, file_menu))
-    menu.add_cascade(label='choose a new image', menu=file_menu)
+    texture_menu = tkinter.Menu(menu, tearoff=0)
+    texture_menu.add_command(label='Reset', command= lambda: reset_image(canvas, menu), state='disable')
+    texture_menu.add_command(label='Choose a new texture', command= lambda: set_texture(canvas, texture_menu), state='disable')
+    texture_menu.add_command(label='Undo', command= lambda: undo_change(canvas, texture_menu), state= 'disable')
+    texture_menu.add_command(label='Redo', command= lambda: redo_change(canvas, texture_menu), state= 'disable')
+    menu.add_cascade(label='Image options', menu=file_menu)
+    menu.add_cascade(label='Texture options', menu = texture_menu)
     menu.add_command(label='Exit', command=window.destroy)
     window.config(menu=menu)
     window.mainloop()
-    
+
 
 if __name__ == "__main__":
     # execute only if run as a script
